d89bcd724296e6996311a2bd98872a88885a55ba
Hirbod Behnam <hirbod.behnam@sharif.edu>
2025-06-03 15:21:35 +0000

    More improvments

--- src/main/java/org/cloudsimplus/examples/lowpower/SLANoDVS.java
+++ src/main/java/org/cloudsimplus/examples/lowpower/SLANoDVS.java
@@ -1,6 +1,5 @@
 package org.cloudsimplus.examples.lowpower;
 
-import org.cloudsimplus.allocationpolicies.migration.VmAllocationPolicyMigrationWorstFitStaticThreshold;
 import org.cloudsimplus.brokers.DatacenterBroker;
 import org.cloudsimplus.brokers.DatacenterBrokerSimple;
 import org.cloudsimplus.builders.tables.CloudletsTableBuilder;
@@ -13,13 +12,13 @@ import org.cloudsimplus.datacenters.DatacenterSimple;
 import org.cloudsimplus.hosts.Host;
 import org.cloudsimplus.hosts.HostSimple;
 import org.cloudsimplus.listeners.CloudletVmEventInfo;
+import org.cloudsimplus.listeners.EventInfo;
 import org.cloudsimplus.power.models.PowerModelHostSimple;
 import org.cloudsimplus.provisioners.ResourceProvisionerSimple;
 import org.cloudsimplus.resources.Pe;
 import org.cloudsimplus.resources.PeSimple;
 import org.cloudsimplus.schedulers.cloudlet.CloudletSchedulerTimeShared;
 import org.cloudsimplus.schedulers.vm.VmSchedulerTimeShared;
-import org.cloudsimplus.selectionpolicies.VmSelectionPolicyMinimumUtilization;
 import org.cloudsimplus.slametrics.SlaContract;
 import org.cloudsimplus.utilizationmodels.UtilizationModel;
 import org.cloudsimplus.utilizationmodels.UtilizationModelDynamic;
@@ -31,10 +30,7 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * This example shows how to do a migration using CpuUtilization threshold
- * defined in an SLA Contract. VM migration is triggered when the CPU metric is violated.
- *
- * @author raysaoliveira
+ * The method proposed in this paper.
  */
 public final class SLANoDVS {
     private final List<Datacenter> datacenterList;
@@ -65,6 +61,7 @@ public final class SLANoDVS {
         createAndSubmitVms(broker);
         createAndSubmitCloudlets(broker);
 
+        simulation.addOnClockTickListener(this::simulationTick);
         simulation.start();
 
         // Fail the tasks after the simulation is done
@@ -136,39 +133,50 @@ public final class SLANoDVS {
             hostList.add(host);
         }
 
-        final var allocationPolicy
-                = new VmAllocationPolicyMigrationWorstFitStaticThreshold(
-                        new VmSelectionPolicyMinimumUtilization(),
-                        contract.getCpuUtilizationMetric().getMaxDimension().getValue());
-        allocationPolicy.setUnderUtilizationThreshold(contract.getCpuUtilizationMetric().getMinDimension().getValue());
-
-        final var dc = new DatacenterSimple(simulation, hostList, allocationPolicy);
-        dc.enableMigrations().setSchedulingInterval(LowPower.SCHEDULE_TIME_TO_PROCESS_DATACENTER_EVENTS);
-        return dc;
+        return new DatacenterSimple(simulation, hostList);
     }
 
     /**
      * When each tasks finishes, we might fail it based on a random number
      */
     private void taskFinishedCallback(CloudletVmEventInfo task) {
-        final ScoredPM pm = (ScoredPM)task.getVm().getHost();
+        final ScoredPM physicalMachine = (ScoredPM) task.getVm().getHost();
+
         if (LowPower.FAILIURE_RNG.eventsHappened()) {
             System.out.println("FAILED task " + task.getCloudlet().getId());
             failedTasks.add(task.getCloudlet());
-            // TODO: Reschedule the task
-            
-            pm.taskDone(false);
+            // We don't reschedule the task in round robin
+
+            physicalMachine.taskDone(false);
         } else {
-            pm.taskDone(true);
+            physicalMachine.taskDone(true);
         }
     }
 
+    /**
+     * This is called on every tick of the simulation
+     * We will update the priority of each task in some ticks.
+     */
+    private void simulationTick(EventInfo info) {
+        if (((long) info.getTime()) % LowPower.T_p == 0) {
+            for (Cloudlet c : cloudletList) {
+                if (c.getStatus() != Status.SUCCESS) {
+                    ((DeadlineTask) c).refreshPriority(info.getTime());
+                }
+            }
+        }
+    }
+
+    /**
+     * A physical machine that holds a score. The score is calculated based on the
+     * number of tasks done successfully and failed tasks.
+     */
     private static final class ScoredPM extends HostSimple {
         private int successfulTasks = 0, failedTasks = 0;
         private final double F_p;
 
         public ScoredPM(final long ram, final long bw, final long storage, final List<Pe> peList, final double mttf) {
-            super(peList);
+            super(ram, bw, storage, peList);
             this.F_p = 1 - Math.exp(-8760 / mttf);
         }
 
@@ -176,6 +184,10 @@ public final class SLANoDVS {
             return getMips() * getPesNumber() * successfulTasks / (successfulTasks + failedTasks) / (1 - F_p);
         }
 
+        /**
+         * When a task is finished this method shall be called
+         * @param successful True if task was sucessful otherwise false
+         */
         public void taskDone(boolean successful) {
             if (successful)
                 successfulTasks++;
@@ -184,6 +196,9 @@ public final class SLANoDVS {
         }
     }
 
+    /**
+     * The scheduler proposed in this paper
+     */
     private static final class TaskScheduler extends DatacenterBrokerSimple {
         public TaskScheduler(final CloudSimPlus simulation) {
             super(simulation, "TaskScheduler");
@@ -201,7 +216,8 @@ public final class SLANoDVS {
                 return Vm.NULL;
 
             // Get the VM which is running on a host that has the best score
-            return getVmExecList().stream().max(Comparator.comparing(c -> ((ScoredPM)(((Vm)c).getHost())).getScore())).get();
+            return getVmExecList().stream().max(Comparator.comparing(c -> ((ScoredPM) (((Vm) c).getHost())).getScore()))
+                    .get();
         }
     }
 
@@ -210,7 +226,7 @@ public final class SLANoDVS {
      */
     private static final class DeadlineTask extends CloudletSimple {
         private final double deadline;
-        
+
         public DeadlineTask(final long id, final long length, final long pesNumber, final double deadline) {
             super(id, length, pesNumber);
             this.deadline = deadline;
@@ -218,6 +234,7 @@ public final class SLANoDVS {
 
         /**
          * Refereshes the priority based on the remaning time to do this task
+         * 
          * @param currentTime The current time of the simulation
          */
         public void refreshPriority(final double currentTime) {
